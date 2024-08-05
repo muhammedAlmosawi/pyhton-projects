6e8cdc5d021dc49e9f5e3497ea03760cc5f5bbda
Mohammed <Muhammadaliraqi52@gmail.com>
2024-08-05 11:23:36 +0300

    here is a hash table

---
+++ data structres/hash_table.py
@@ -0,0 +1,39 @@
+class hash_not_map():
+    def __init__(self, size):
+        self.size = size
+        self.keys = [None] * size
+        self.values = self.keys
+
+    def hash_function(self, key):
+        return hash(key) % self.size
+    
+    def put(self, key, value):
+        index = self.hash_function(key)
+
+        while self.keys[index] != None:
+            if self.keys == key:
+                self.values[index] = value
+                return 
+            index = (index + 1) % self.size
+
+
+        self.keys[index] = key
+        self.values[index] = value        
+
+    def get(self, key):
+        index = self.hash_function(key)
+
+        while self.keys[index] != None:
+            if self.keys[index] != None:
+                return self.values[index]
+            index = self.hash_function(key + 1)
+
+        return None
+    
+
+hash_table = hash_not_map(5)
+hash_table.put(12, 5)
+hash_table.put(22, 70)
+
+print(hash_table.get(12))
+print(hash_table.get(22))
