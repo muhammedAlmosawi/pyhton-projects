095c99d19eb4863a6c8b55598cb901bd896b9aeb
Mohammed <Muhammadaliraqi52@gmail.com>
2024-08-08 10:48:25 +0300

    here I made a deque cuz why not

---
+++ data structres/linked_list.py
@@ -0,0 +1,7 @@
+from dstructure.DLL import DLL
+linked_list = DLL()
+linked_list.insert(40)
+linked_list. insert(10)
+linked_list.insert(10)
+linked_list.insert(50)
+linked_list.print()
---
+++ data structres/stack.py
@@ -0,0 +1,63 @@
+import logging
+from collections import deque
+
+logger = logging.getLogger(__name__)
+
+class deque:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.deque = []
+        self.LastElement = 0
+        self.FirstElement = 0
+    
+    def isEmpty(self):
+        return self.LastElement == 0
+        
+    def isFull(self):
+        return self.LastElement >= self.capacity
+
+    def push(self, item):
+        if self.isFull():
+            logger.error("the deque is full")
+        else:
+            self.deque.append(item)
+            self.LastElement += 1
+            print(f"{item} has been pushed to the top of the deque")
+
+    def pushLeft(self, item):
+        if self.isFull():
+            logger.error("the deque is full!!")
+        else:
+            self.deque.insert(self.FirstElement, item)
+            self.LastElement += 1
+            print(f"{item} has been added to the start of the deque")
+
+    def pop(self):
+        if self.isEmpty():
+            logger.error("the deque is empty")
+        else:
+            self.deque.pop
+            self.LastElement -= 1
+            print(f"the last element of the deque has been removed")
+
+    def popLeft(self):
+        if self.isEmpty:
+            logger.error("the deque is empty")
+        else:
+            self.deque.pop(self.FirstElement)
+            self.LastElement -= 1
+            print("the first element has been removed")
+
+    def peek(self):
+        if self.isEmpty():
+            logger.error("the deque is empty (in the peek function)")
+        else:
+            print(f"the last element of the deque is {self.deque[self.LastElement - 1]}")
+        
+d = deque(10)
+d.pop()
+d.push(17)
+d.pushLeft(15)
+d.push(3)
+d.pushLeft(4)
+d.peek()
